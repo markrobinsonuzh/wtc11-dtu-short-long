6d46e716ba71aeb120fa4e4791179018e10a9a2b
slightlier <slightlier@gmail.com>
2023-11-10 15:15:32 +0100

    feat: Prepare for single-cell

--- workflow/scripts/run_bambu.R
+++ workflow/scripts/run_bambu.R
@@ -1,6 +1,6 @@
 run_bambu <- function(reads, annotations, genome, discovery, output_directory, ncore, lowMemory = TRUE) {
     library(bambu)
-    bambuAnnotations <- prepareAnnotations(annotations)
+    bambuAnnotations <- bambu::prepareAnnotations(annotations)
     bambu_result <- bambu::bambu(reads = reads, annotations = bambuAnnotations, genome = genome, discovery = discovery,
                                  ncore = ncore, lowMemory = lowMemory
     )
@@ -11,8 +11,8 @@ run_bambu(
      reads = unlist(strsplit(snakemake@input[["reads"]], ",")),
      annotations = snakemake@input[["annotation"]],
      genome = snakemake@input[["reference"]],
-     discovery = FALSE,
-     output_directory = snakemake@output,
+     discovery = snakemake@params[["discovery"]],
+     output_directory = snakemake@params[["output_dir"]],
      ncore = snakemake@threads,
-     lowMemory = FALSE
+     lowMemory = TRUE
      )
